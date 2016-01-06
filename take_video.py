d119f09d4a01dc89554d1ecd20bd2d721c19f879
Sami Alsheikh <alsheikh@mit.edu>
2016-01-05 20:56:34 -0500

    Made meaningful changes

--- take_video.py
+++ take_video.py
@@ -4,23 +4,25 @@ import cv2
 
 cap = cv2.VideoCapture(-1)
 
-darker_green = np.uint8([[[65, 83, 62]]])
-lighter_green = np.uint8([[78, 92, 67]])
+# darker_green = np.uint8([[[65, 83, 62]]])
+# lighter_green = np.uint8([[78, 92, 67]])
 
 while(True):
 
-	# Might want to check cap.isOpened()
+    # Might want to check cap.isOpened()
 
-	# Capture frame-by-frame
-	success, frame = cap.read()
+    # Capture frame-by-frame
+    success, frame = cap.read()
 
-	# Our operations on the frame come here
-	hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    # Our operations on the frame come here
+    # hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    hsv_frame = frame
 
-	# Display the resulting frame
-	cv2.imshow('iRobot!!! hahahahaha', hsv_frame)
-	# if cv2.waitKey(1) & 0xFF == ord('q'):
-	#     break
+    # Display the resulting frame
+    cv2.imshow('iRoboCop - Your computer is infected!!', hsv_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 # If you want to save the image
 # cv2.imwrite('first_image.png', frame)
